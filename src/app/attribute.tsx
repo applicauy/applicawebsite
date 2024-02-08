23a7cc1943b75fe25d3bc0e72f0d513b6ce06c91
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-08 14:50:14 -0300

    Added `Attribute` component
    To be used in the services section.

---
+++ src/app/attribute.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from "react";
+import Image from "next/image";
+
+export default function Attribute({
+  icon,
+  children,
+}: PropsWithChildren<{ icon: any }>) {
+  return (
+    <div className="flex items-center">
+      <Image src={icon} alt="" width={80} />
+      <h5>{children}</h5>
+    </div>
+  );
+}
