a6f6d0238670aaf658ff9b4e8c441f5f0aba42a2
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-09 16:42:36 -0300

    Reduced gap between sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <NavBar />
 
       <main
-        className={`${apexFont.className} flex min-h-screen flex-col items-center justify-between p-24 gap-48`}
+        className={`${apexFont.className} flex min-h-screen flex-col items-center justify-between p-24 gap-36`}
       >
         <LandingSection />
 
