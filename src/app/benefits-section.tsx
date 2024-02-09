8e5becc26151510b5d0c090f4935c9eb041ef94c
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-09 16:43:23 -0300

    Implementation of Carousel on Benefits section

--- src/app/benefits-section.tsx
+++ src/app/benefits-section.tsx
@@ -1,8 +1,8 @@
 import { avigeaFont } from "@/assets/fonts";
 import Section from "@/components/section";
-import Image from "next/image";
 import placeholderImg from "@/assets/placeholder-img-2.png";
 import Benefit from "./benefit";
+import Carousel from "./carousel";
 
 const BENEFITS = [
   {
@@ -29,29 +29,33 @@ const BENEFITS = [
 
 export default function BenefitsSection() {
   return (
-    <Section classNameInner="flex gap-8 items-center">
-      <div className="flex flex-col w-2/4 gap-12">
-        <h2 className="md:max-w-[80%]">
-          Let&apos;s find your next{" "}
-          <strong className={`font-normal ${avigeaFont.className}`}>
-            most valuable player.
-          </strong>
-        </h2>
+    <Section classNameInner="flex flex-col gap-36">
+      <div className="flex gap-8 items-center">
+        <div className="flex flex-col w-2/4 gap-12">
+          <h2 className="md:max-w-[80%]">
+            Let&apos;s find your next{" "}
+            <strong className={`font-normal ${avigeaFont.className}`}>
+              most valuable player.
+            </strong>
+          </h2>
 
-        <h3>
-          Long-term partnership requires flexible service. Since 2015, Applica
-          Corp&apos;s has been dedicated to helping companies scale their
-          business and take it to the next level.
-        </h3>
+          <h3>
+            Long-term partnership requires flexible service. Since 2015, Applica
+            Corp&apos;s has been dedicated to helping companies scale their
+            business and take it to the next level.
+          </h3>
+        </div>
+
+        <div className="w-2/4 grid grid-cols-2">
+          {BENEFITS.map((b, index) => (
+            <Benefit key={index} imageSrc={b.image} imageAlt={b.alt}>
+              {b.description}
+            </Benefit>
+          ))}
+        </div>
       </div>
 
-      <div className="w-2/4 grid grid-cols-2">
-        {BENEFITS.map((b, index) => (
-          <Benefit key={index} imageSrc={b.image} imageAlt={b.alt}>
-            {b.description}
-          </Benefit>
-        ))}
-      </div>
+      <Carousel />
     </Section>
   );
 }
