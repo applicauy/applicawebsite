c86890e8520c0a2515389e1073a59e64076b8641
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-14 13:18:56 -0300

    Finished implementation of `Process` section

--- src/app/process-section.tsx
+++ src/app/process-section.tsx
@@ -1,15 +1,73 @@
 import Section from "@/components/section";
-import { avigeaFont } from '@/assets/fonts';
+import { avigeaFont } from "@/assets/fonts";
+import ProcessItem, { ContentPosition } from "./process-item";
+import ProcessItemBody from "./process-item-body";
 
 export default function ProcessSection() {
     return (
-      <Section>
-        <h2>
-          Hiring{" "}
-          <strong className={`font-normal ${avigeaFont.className}`}>
-            process
-          </strong>
-        </h2>
-      </Section>
+        <Section classNameInner="w-full">
+            <h2 className="mb-12 text-center">
+                Hiring{" "}
+                <strong className={`font-normal ${avigeaFont.className}`}>
+                    process
+                </strong>
+            </h2>
+
+            <div className="flex flex-col w-full">
+                <ProcessItem iconSrc="" contentPosition={ContentPosition.Left}>
+                    <ProcessItemBody title="Requirements analysis">
+                        Let us know your requirements: role / skills / languages
+                        & timezone.
+                    </ProcessItemBody>
+                </ProcessItem>
+
+                <ProcessItem iconSrc="" contentPosition={ContentPosition.Right}>
+                    <ProcessItemBody title="Screening">
+                        +4,000 pre-vetted profiles in the Talent Pool are
+                        reviewed by our team of recruiters.
+                    </ProcessItemBody>
+                </ProcessItem>
+
+                <ProcessItem iconSrc="" contentPosition={ContentPosition.Left}>
+                    <ProcessItemBody title="Profiles selection">
+                        Best fitted to your requirements, ensuring their
+                        technical quality.
+                    </ProcessItemBody>
+                </ProcessItem>
+
+                <ProcessItem iconSrc="" contentPosition={ContentPosition.Right}>
+                    <ProcessItemBody title="Candidates deliver">
+                        Meet a tailor-made group of applicants who are in line
+                        with the initial requirements.
+                    </ProcessItemBody>
+                </ProcessItem>
+
+                <ProcessItem iconSrc="" contentPosition={ContentPosition.Left}>
+                    <ProcessItemBody title="Interview & confirmation">
+                        1:1 interviews with the shortlisted profiles for you to
+                        decide on the best fit.
+                    </ProcessItemBody>
+                </ProcessItem>
+
+                <ProcessItem iconSrc="" contentPosition={ContentPosition.Right}>
+                    <ProcessItemBody title="On boarding">
+                        Support and guidance during induction into the role,
+                        project and company culture.
+                    </ProcessItemBody>
+                </ProcessItem>
+
+                <ProcessItem
+                    iconSrc=""
+                    contentPosition={ContentPosition.Left}
+                    customLineHeight="3rem"
+                    dashedEndOfLine={true}
+                    dashedEndOfLineHeight="2rem"
+                >
+                    <ProcessItemBody title="EX & monitoring">
+                        Performance and sactisfaction tracking of stakeholders.
+                    </ProcessItemBody>
+                </ProcessItem>
+            </div>
+        </Section>
     );
 }
