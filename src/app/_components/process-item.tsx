917eded3318dad2f33f39cda7bb8f90409b69c86
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-15 16:10:12 -0300

    Improved style of component `ProcessItem`

--- src/app/_components/process-item.tsx
+++ src/app/_components/process-item.tsx
@@ -1,5 +1,4 @@
-import Image, { StaticImageData } from "next/image";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, ReactNode } from "react";
 
 export enum ContentPosition {
     Left,
@@ -8,22 +7,30 @@ export enum ContentPosition {
 
 export default function ProcessItem({
     children,
-    iconSrc,
+    customIcon,
     colored,
     contentPosition,
     customLineHeight,
     dashedEndOfLine,
     dashedEndOfLineHeight,
+    className,
+    lineContainerClassName,
 }: PropsWithChildren<{
-    iconSrc: string | StaticImageData;
+    customIcon?: ReactNode;
     colored?: boolean;
     contentPosition: ContentPosition;
     customLineHeight?: string;
     dashedEndOfLine?: boolean;
     dashedEndOfLineHeight?: string;
+    className?: string;
+    lineContainerClassName?: string;
 }>) {
     return (
-        <div className={`flex gap-20 w-full min-h-36`}>
+        <div
+            className={`flex gap-20 w-full min-h-36${
+                className ? " " + className : ""
+            }`}
+        >
             {/* Content box */}
             <div className="w-full text-right py-2">
                 {contentPosition === ContentPosition.Left && children}
@@ -31,22 +38,18 @@ export default function ProcessItem({
 
             {/* Central line box */}
             <div className="min-h-full w-fit flex flex-col items-center">
-                {iconSrc ? (
-                    <Image src={iconSrc} alt="" />
-                ) : (
-                    <div className="dot bg-white"></div>
-                )}
+                {customIcon ? customIcon : <div className="dot bg-white"></div>}
 
                 <div
-                    className="w-[2px] bg-white"
+                    className="w-[1px] bg-white min-h-20"
                     style={{
-                        minHeight: customLineHeight ?? "100%",
+                        height: customLineHeight ?? "100%",
                     }}
                 ></div>
 
                 {dashedEndOfLine && (
                     <div
-                        className="border border-white border-dashed"
+                        className="border-r border-white border-dashed"
                         style={{ minHeight: dashedEndOfLineHeight }}
                     ></div>
                 )}
