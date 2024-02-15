586ffc674f626b7177df17b96a74f4236f50e595
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-15 16:15:48 -0300

    Removed unused parameter

--- src/app/_components/process-item.tsx
+++ src/app/_components/process-item.tsx
@@ -14,7 +14,6 @@ export default function ProcessItem({
     dashedEndOfLine,
     dashedEndOfLineHeight,
     className,
-    lineContainerClassName,
 }: PropsWithChildren<{
     customIcon?: ReactNode;
     colored?: boolean;
@@ -23,7 +22,6 @@ export default function ProcessItem({
     dashedEndOfLine?: boolean;
     dashedEndOfLineHeight?: string;
     className?: string;
-    lineContainerClassName?: string;
 }>) {
     return (
         <div
