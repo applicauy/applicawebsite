dfb5bf6fb1f79a180aabd82b522963af7610e57d
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-22 15:47:17 -0300

    Changed `div` by a `section`

--- src/app/_sections/lets-talk-section.tsx
+++ src/app/_sections/lets-talk-section.tsx
@@ -2,7 +2,7 @@ import { avigeaFont } from "@/assets/fonts";
 import Button from "../../components/button";
 export default function LetsTalkSection() {
     return (
-        <div className="flex justify-center items-center min-h-80 w-full gap-8 bg-gradient-to-br from-violet-900 from-15% via-fuchsia-700 to-[--background-color] to-85%">
+        <section className="flex justify-center items-center min-h-80 w-full gap-8 bg-gradient-to-br from-violet-900 from-15% via-fuchsia-700 to-[--background-color] to-85%">
             <h3>
                 Got a{" "}
                 <strong className={`font-normal ${avigeaFont.className}`}>
@@ -11,6 +11,6 @@ export default function LetsTalkSection() {
                 ?
             </h3>
             <Button>Let&apos;s talk!</Button>
-        </div>
+        </section>
     );
 }
