b9a58d6475532d279ef182ec2c239c64ffc22b83
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-23 17:41:04 -0300

    Added component `TextArea`

---
+++ src/components/textarea.tsx
@@ -0,0 +1,32 @@
+import { ChangeEventHandler } from "react";
+
+export default function TextArea({
+    id,
+    name,
+    placeholder,
+    required,
+    cols = 30,
+    rows = 10,
+    onChange,
+}: {
+    id?: string;
+    name?: string;
+    placeholder?: string;
+    required?: boolean;
+    cols?: number;
+    rows?: number;
+    onChange?: ChangeEventHandler<HTMLTextAreaElement>;
+}) {
+    return (
+        <textarea
+            name={name}
+            id={id}
+            cols={cols}
+            rows={rows}
+            placeholder={placeholder}
+            required={required}
+            onChange={onChange}
+            className="mt-4 block w-full px-0.5 border border-[--highlight-color] focus:ring-0 focus:border-[--highlight-color] focus:border-2 bg-transparent rounded-br-[50px] resize-none"
+        />
+    );
+}
