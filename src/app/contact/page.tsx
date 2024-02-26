ca062993b981cd22c9629703fc64b55abb61c9ab
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-26 14:45:47 -0300

    Partially implemented Contact view

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import FormSection from "./_sections/form-section";
+export default function Contact() {
+    return (
+        <main className="container mx-auto min-h-screen flex text-white py-48">
+            <div></div>
+
+            <div className="w-1/2">
+                <FormSection />
+            </div>
+        </main>
+    );
+}
