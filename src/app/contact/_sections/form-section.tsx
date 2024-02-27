0eab90023709c31d3cfc2804137197efc29e1666
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-02-27 17:42:41 -0300

    Implemented usage of server side action and error rendering

--- src/app/contact/_sections/form-section.tsx
+++ src/app/contact/_sections/form-section.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Input from "@/components/input";
 import Label from "@/components/label";
 import TextArea from "../../../components/textarea";
+import { useFormState, useFormStatus } from "react-dom";
+import { handleContactRequest, FormDataErrors } from "./actions";
+import Errors from "../_components/errors";
 
 const REFERRALS_VALUES = [
     "Google search",
@@ -10,40 +15,79 @@ const REFERRALS_VALUES = [
     "Referral",
 ];
 
-export default function FormSection() {
-    const handleContactRequest = async (formData: FormData) => {
-        "use server";
+const initialState: FormDataErrors = {
+    formErrors: [],
+    fieldErrors: {},
+};
 
-        // TODO Validate data and send the email
-    };
+export default function FormSection() {
+    const status = useFormStatus();
+
+    const [state, formAction] = useFormState(
+        handleContactRequest,
+        initialState
+    );
 
     return (
-        <form action={handleContactRequest} className="grid grid-cols-1 gap-8">
-            <Label title="Name" required>
-                <Input name="name" type="text" required />
-            </Label>
+        <form action={formAction} className="grid grid-cols-1 gap-8">
+            <div>
+                <Label title="Name" required>
+                    <Input name="name" type="text" required />
+                </Label>
 
-            <Label title="Work email" required>
-                <Input id="email" name="email" type="email" required />
-            </Label>
+                {state?.fieldErrors.name && (
+                    <Errors errors={state?.fieldErrors.name} />
+                )}
+            </div>
 
-            <Label title="Phone">
-                <Input id="phone" name="phone" type="tel" />
-            </Label>
+            <div>
+                <Label title="Work email" required>
+                    <Input name="email" type="email" required />
+                </Label>
 
-            <Label title="How did you heard about us?" required>
-                <Input
-                    id="referral"
-                    name="referral"
-                    type="text"
-                    datalistValues={REFERRALS_VALUES}
-                    required
-                />
-            </Label>
+                {state?.fieldErrors.email && (
+                    <Errors errors={state?.fieldErrors.email} />
+                )}
+            </div>
 
-            <Label title="Tell us about your needs" required>
-                <TextArea />
-            </Label>
+            <div>
+                <Label title="Phone">
+                    <Input name="phone" type="tel" />
+                </Label>
+
+                {state?.fieldErrors.phone && (
+                    <Errors errors={state?.fieldErrors.phone} />
+                )}
+            </div>
+
+            <div>
+                <Label title="How did you heard about us?" required>
+                    <Input
+                        name="referral"
+                        type="text"
+                        datalistValues={REFERRALS_VALUES}
+                        required
+                    />
+                </Label>
+
+                {state?.fieldErrors.referral && (
+                    <Errors errors={state?.fieldErrors.referral} />
+                )}
+            </div>
+
+            <div>
+                <Label title="Tell us about your needs" required>
+                    <TextArea name="message" />
+                </Label>
+
+                {state?.fieldErrors.message && (
+                    <Errors errors={state?.fieldErrors.message} />
+                )}
+            </div>
+
+            <button type="submit" disabled={status.pending}>
+                Submit
+            </button>
         </form>
     );
 }
