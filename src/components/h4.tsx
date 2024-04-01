47b01aee0dc1a2466ac9a1a5ecb5b8062928e8c3
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-04-01 15:28:50 -0300

    Add H4 component

---
+++ src/components/h4.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren } from "react";
+
+const H4 = ({
+    children,
+    className,
+}: PropsWithChildren<{ className?: string }>) => {
+    return (
+        <h4
+            className={
+                "text-3xl leading-none" + (className ? " " + className : "")
+            }
+        >
+            {children}
+        </h4>
+    );
+};
+
+export default H4;
