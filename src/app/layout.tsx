a48287ac809cef1be0fda636f0a0999fdc75b413
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-04-01 15:30:51 -0300

    Usage of SASS instead of CSS for global styles
    Also, removed some redundant styling now set by using tailwind directly.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import NavBar from "./_components/navbar";
 import Footer from "./_components/footer";
 
