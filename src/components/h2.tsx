aa04df93f18ff10263037ad375616b3fe6a1f855
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-04-01 14:39:31 -0300

    Add H2 component

---
+++ src/components/h2.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from "react";
+
+const H2 = ({
+    children,
+    className,
+}: PropsWithChildren<{ className?: string }>) => {
+    return (
+        <h2 className={"text-4xl md:text-6xl leading-none" + (className ? " " + className : "")}>
+            {children}
+        </h2>
+    );
+};
+
+export default H2;
