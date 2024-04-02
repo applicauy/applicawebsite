588559f5a9779ffa1db9b45bb483607ec1b338a9
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-04-02 16:58:41 -0300

    Adaptation to mobile of Services section

--- src/app/_sections/services-section.tsx
+++ src/app/_sections/services-section.tsx
@@ -15,7 +15,7 @@ import H2 from "@/components/h2";
 
 export default function ServicesSection() {
     return (
-        <Section className="flex flex-col gap-20">
+        <Section className="flex flex-col md:gap-20">
             <motion.div
                 className="flex flex-col md:flex-row gap-8"
                 initial={{ opacity: 0, x: "-20px" }}
@@ -47,13 +47,13 @@ export default function ServicesSection() {
                     />
                 </div>
 
-                <div className="flex flex-col gap-8 md:w-2/4 text-center md:text-end">
-                    <H3>
+                <div className="flex flex-col md:gap-8 md:w-2/4 text-center md:text-end my-8 md:my-0">
+                    <H3 className="mb-14 md:mb-0">
                         We specialize in IT staff augmentation services, using
                         an agile model to deliver talent in only 72 hours.
                     </H3>
 
-                    <H2>
+                    <H2 className="max-w-[70%] md:max-w-full mx-auto md:mx-0 mb-3 mb:md-0">
                         Ready to{" "}
                         <strong
                             className={`font-normal ${avigeaFont.className}`}
@@ -62,9 +62,7 @@ export default function ServicesSection() {
                         </strong>
                     </H2>
 
-                    <H3 className="text-highlight">
-                        We make it happen
-                    </H3>
+                    <H3 className="text-highlight">We make it happen</H3>
                 </div>
             </motion.div>
 
@@ -73,7 +71,7 @@ export default function ServicesSection() {
                 whileInView={{ opacity: 1, x: "0px" }}
                 viewport={{ once: true }}
                 transition={{ duration: 0.6, delay: 0.8 }}
-                className="flex flex-col md:flex-row gap-6"
+                className="flex flex-col md:flex-row gap-6 max-w-[80%] md:max-w-full mx-auto md:mx-0 mb-14 md:mb-0"
             >
                 <Attribute icon={searchIcon}>
                     Pre-vetted & tailor match nearshore IT talent you can count
@@ -96,7 +94,7 @@ export default function ServicesSection() {
                 whileInView={{ opacity: 1, x: "0px" }}
                 viewport={{ once: true }}
                 transition={{ duration: 0.6, delay: 0.8 }}
-                className="flex flex-col gap-6 px-36 pt-16 pb-9 rounded-full border text-center items-center service-gradient"
+                className="flex flex-col gap-6 px-10 md:px-36 pt-8 md:pt-16 pb-7 md:pb-9 rounded-3xl md:rounded-full border text-center items-center bg-gradient-to-br from-indigo-700 via-purple-600 to-pink-600"
             >
                 <H2 className="font-medium">
                     Find the{" "}
@@ -106,7 +104,7 @@ export default function ServicesSection() {
                     for your venture
                 </H2>
 
-                <H3>
+                <H3 className="text-md">
                     Whether you need a specialized skillset, a temporary highly
                     qualified developer, a Senior team lead, or just more hands
                     on deck, Our IT talents are here to help.
