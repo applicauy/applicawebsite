d2ce1bd7465cf04bca66a4bae2d7ea41c1c267f5
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-08-08 21:45:41 -0300

    Comment about static compilation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Esto habilita la compilación estática al ejecutar `npm run build`. No funciona con
+    // esta solución por el uso de funcionalidades dinámicas (las cookies por ejemplo).
+    // output: "export"
+};
 
 export default nextConfig;
