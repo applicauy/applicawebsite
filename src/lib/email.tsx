b2ae3e226a8af89f1cb3bc88818a1db3d6feadaf
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-08-10 23:16:14 -0300

    Added utility to send emails on the backend

--- src/lib/email.tsx
+++ src/lib/email.tsx
@@ -1,10 +1,36 @@
+"use server";
+
+import nodemailer from "nodemailer";
+import { Headers } from "nodemailer/lib/mailer";
+
 /**
  * Represents the payload for an email.
  */
 export type EmailPayload = {
-    email: string;
-    subject?: string;
-    html: string;
+    subject: string;
+    html?: string;
     text?: string;
     headers?: Headers;
+};
+
+const SMTP_OPTIONS = {
+    host: process.env.SMTP_HOST,
+    port: parseInt(process.env.SMTP_PORT || "2525"),
+    secure: false,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+    },
+};
+
+export const sendEmail = async (payload: EmailPayload) => {
+    const transporter = nodemailer.createTransport({
+        ...SMTP_OPTIONS,
+    });
+
+    await transporter.sendMail({
+        ...payload,
+        from: process.env.SMTP_FROM_EMAIL,
+        to: process.env.SMTP_TO_EMAIL,
+    });
 };
