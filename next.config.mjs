efe0e4e66e5f4c1233f3a86ccdb9d0070b59d301
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-08-11 00:42:58 -0300

    Set option `output` to `standalone`

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     // Esto habilita la compilación estática al ejecutar `npm run build`. No funciona con
     // esta solución por el uso de funcionalidades dinámicas (las cookies por ejemplo).
     // output: "export"
+    // Teóricamente esto es requerido para publicarlo en Azure, ya que se hace uso del archivo
+    // server.js que produce para tenerlo como punto de inicio con nodejs.
+    output: "standalone"
 };
 
 export default nextConfig;
