3019fcdacaf9c296bd9f59723d36d5c86f59ef18
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-10-07 21:48:13 -0300

    Fixed usage of GA through GTM

--- src/app/(mobile)/layout.tsx
+++ src/app/(mobile)/layout.tsx
@@ -4,17 +4,23 @@ import "./styles/mobile-layout.scss";
 import MobileNavbar from "./_components/mobile-navbar";
 import MobileFooter from "./_components/mobile-footer";
 import { ReactNode } from "react";
+import TrackPageView from "@/components/track-page-view";
+import { GoogleTagManager } from "@next/third-parties/google";
+
+const GTM_ID = "GTM-PQ3DNDZ";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const MobileLayout = ({ children }: { children: ReactNode }) => {
-
     return (
         <html lang="en">
             <body className={`mobile-layout ${inter.className}`}>
                 <MobileNavbar />
                 {children}
                 <MobileFooter />
+
+                <GoogleTagManager gtmId={GTM_ID} />
+                <TrackPageView />
             </body>
         </html>
     );
