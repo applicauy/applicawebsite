e475e722b4896cb42ea101a09a39918005aec7a5
Mauro Rocca <mauroroccaf@gmail,com>
2024-10-23 23:29:54 -0300

    delete references for layout2 and mobiile-home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
-import { cookies, headers } from "next/headers";
-import LandingSection from "./_sections/landing-section";
-import AboutUs from "./_sections/about-us";
-import BenefitsSection from "./_sections/benefits-section";
-import { apexFont } from "@/assets/fonts";
-import ProcessSection from "./_sections/process-section";
-import TalentOnDemand from "./_sections/talent-on-demand";
-import MobileHome from "./(mobile)/mobile-home";
-import LatestPosts from "./_sections/latest-posts";
-import BlogSection from "./_sections/blog-section";
+import { headers } from "next/headers";
 import MobileDetect from "mobile-detect";
 import PageClientWrapper from "./page-client-wrapper";
 
