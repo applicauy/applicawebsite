7721cabe1009ede4b50ae91d257e32f260c99fb5
Mauro Rocca <mauroroccaf@gmail.com>
2025-05-27 15:20:56 -0300

    meta-tags duplicates: fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import { headers } from "next/headers";
 import MobileDetect from "mobile-detect";
 import PageClientWrapper from "./page-client-wrapper";
-import isMobile from 'react-device-detect';
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Applica Corp | Tailored software development for your growth',
+  description: 'We are a software company focused on saving you time and delivering peace of mind. Build your dream team with top nearshore IT talent in only 72 hs.',
+  keywords: ['staff augmentation', 'software', 'company', 'team', 'talent'],
+  alternates: {
+    canonical: 'https://applicacorp.com/',
+  },
+};
 
 export default function Home() {
 
